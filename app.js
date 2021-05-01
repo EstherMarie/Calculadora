ef8f23395d0204c65aabe877e450b022c09eb815
EstherMarie <24815585+EstherMarie@users.noreply.github.com>
2021-04-30 21:31:32 -0300

    calculadora e histórico ok

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const buttons = document.querySelectorAll('button');
 let visor = document.querySelector('.visor');
 visor.style.cssText = 'text-align: right;';
 
-
-let historico = document.querySelector('.historico')
+let historico = document.querySelector('.historico');
 
 let primeiroValor;
 let segundoValor;
@@ -79,52 +78,79 @@ window.addEventListener('keydown', (e) => {
 		case 13:
 			key = '=';
 			// console.log(key);
-			visor.value = Function('"use strict";return (' + visor.value + ')')();
-			segundoValor = undefined;
-			opr = undefined;
+			let r1 = Function('"use strict";return (' + visor.value + ')')();
+
+			let divOperacao = document.createElement('div');
+			divOperacao.classList.add('operacao');
+			let divResultado = document.createElement('div');
+			divResultado.classList.add('resultado');
+
+			divOperacao.textContent = visor.value + ' = ';
+			divResultado.textContent = r1;
+
+			divOperacao.insertAdjacentElement('beforeend', divResultado);
+			historico.insertAdjacentElement('afterend', divOperacao);
+
+			visor.value = r1;
+			segundoValor = opr = undefined;
 			break;
 	}
 });
 
 buttons.forEach((button) => {
 	button.addEventListener('click', () => {
+		function calculaResultado() {
+			console.log(primeiroValor, opr, segundoValor);
+			let resultado = calculo(primeiroValor, segundoValor, opr);
+
+			let divOperacao = document.createElement('div');
+			divOperacao.classList.add('operacao');
+			let divResultado = document.createElement('div');
+			divResultado.classList.add('resultado');
+
+			divOperacao.textContent = visor.value + ' = ';
+			divResultado.textContent = resultado;
+
+			divOperacao.insertAdjacentElement('beforeend', divResultado);
+			historico.insertAdjacentElement('beforeend', divOperacao);
+			segundoValor = opr = undefined;
+			visor.value = primeiroValor = resultado;
+		}
+
 		if (button.value == 'C') {
 			visor.value = '';
 			primeiroValor = segundoValor = opr = undefined;
 		} else if (button.classList.contains('operador')) {
-			if (segundoValor == undefined) {
-				if (opr == undefined) {
-					primeiroValor = Number(visor.value);
+			if (opr == undefined) {
+				primeiroValor = Number(visor.value);
+				opr = button.value;
+			} else if (opr != undefined) {
+				// se possui algo a direito do opr => calculo()
+				if (visor.value.slice(visor.value.lastIndexOf(opr))) {
+					segundoValor = Number(visor.value.slice(visor.value.indexOf(opr) + 1));
+					calculaResultado();
 					opr = button.value;
-				} else if (opr != undefined) {
+				}
+
+				// senão trocar opr antigo pelo opr digitado
+				else if (visor.value.slice(visor.value.lastIndexOf(opr)) == false) {
 					primeiroValor = Number(visor.value.slice(0, visor.value.lastIndexOf(opr)));
 					visor.value[visor.value.lastIndexOf(opr)] = button.value;
 					opr = button.value;
 				}
-			} if (primeiroValor && segundoValor && opr != undefined) {
-				visor.value = primeiroValor = calculo(primeiroValor,segundoValor,opr);
-				segundoValor = opr = undefined;
-			} 
+			}
 
 			visor.value = primeiroValor + opr;
 			console.log(visor.value, primeiroValor, opr);
+
 		} else if (button.value == '=') {
 			segundoValor = Number(visor.value.slice(visor.value.indexOf(opr) + 1));
-			console.log(segundoValor);
 
-			
-			if (primeiroValor && segundoValor != undefined) {
-				let resultado = calculo(primeiroValor,segundoValor,opr);
-
-				let divResultado = document.createElement('div')
-				divResultado.classList.add('resultado')
-
-				divResultado.textContent = visor.value + ' = ' + resultado
-				historico.insertAdjacentElement('afterend', divResultado)
-				visor.value = primeiroValor = resultado
-				segundoValor = opr = undefined;
-			} else {
+			if (primeiroValor == undefined || segundoValor == undefined || opr == undefined) {
+				console.log(primeiroValor, opr, segundoValor);
 				visor.value = 'Operação inválida';
+			} else if (primeiroValor != undefined && segundoValor != undefined) {
+				calculaResultado();
 			}
 		} else {
 			visor.value += button.value;
@@ -142,6 +168,10 @@ function calculo(valor1, valor2, operando) {
 	} else if (operando == '/') {
 		return valor1 / valor2;
 	}
+}
+
+function limparHistorico() {
+	historico.textContent = '';
 }
 
 // Sobre o método slice:
